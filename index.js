f2a707dc9e3623508c1634337cba5ddca9f73661
Cedric David <cedric.david@btopenworld.com>
2018-10-28 00:06:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+  const src = document.getElementById("repository-template").innerHTML
+  const template = Handlebars.compile(src)
+  const repoList = template(repos)
+  document.getElementById("repositories").innerHTML = repoList
+}
